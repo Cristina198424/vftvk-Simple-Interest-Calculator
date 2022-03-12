8e43d823f681ab6bec7bcd72aa8f1bddb10c28c6
Cristina198424 <crishoulla@msn.com>
2022-03-12 16:50:08 +0000

    commiy

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerText="If you deposit " +principal +
+    " at an interest rate of " + rate +
+    "%. You will receive an amount of "+ amount + " in the year " + year    ;
+
+}
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
